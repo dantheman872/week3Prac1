00aa7d141f2557d82374fc5ad6065b8695e68888
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2025-10-08 11:30:24 +0100

    Update sketch.js

--- exercise2-3/sketch.js
+++ exercise2-3/sketch.js
@@ -1,3 +1,6 @@
+let cX = 300
+let cY = 300
+
 function setup(){
 
     createCanvas(600,600)
@@ -5,12 +8,21 @@ function setup(){
 
 function draw(){
 
-    background(0)
-    fill(255)
-    square(100,100, 50)
+    background(255)
+    rectMode(CENTER)
+    if (mouseIsPressed && mouseX < cX+25 && mouseX > cX-25 && mouseY < cY+25 && mouseY > cY-25 ){
 
-    if (mouseIsPressed()){
+        fill(0,170,180)
+        strokeWeight(5)
+        stroke(255,50,50,220)
+        square(mouseX,mouseY,200)
+        cY = mouseY
+        cX = mouseX
         
-        
+    } else {
+
+        fill(50,220,230)
+        strokeWeight(1)
+        square(cX,cY,200)
     }
 }
